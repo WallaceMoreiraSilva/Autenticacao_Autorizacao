180db29702666c0fdc4bf0b4e07d2f331576e6c3
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2020-10-05 14:45:10 -0300

    Primeiro Commit sobre Autenticação e Autorização via Token (Bearer e JWT)

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using Shop.Respositories;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate ([FromBody]UserModel model )
+        {
+            UserRepository userRepository = new UserRepository();
+
+            var user = userRepository.Get(model.UserName, model.Password);
+  
+            if (user == null)
+                return NotFound(new {message = "Usuário ou Senha invalidos" });
+
+            var token = TokenService.GenerationToken(user);
+
+            user.Password = string.Empty;
+
+            return new 
+            { 
+                user = user,
+                token = token   
+            };
+
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => string.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee, manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+
+    }
+}
---
+++ Shop/Respositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Respositories
+{
+    public class UserRepository
+    {
+        public UserModel Get (string username, string password)
+        {
+            var users = new List<UserModel>();
+
+            users.Add(new UserModel
+            {
+                Id = 1,
+                UserName = "batman",
+                Password = "batman",
+                Role = "manager"
+            });
+
+            users.Add(new UserModel
+            {
+                Id = 2,
+                UserName = "robin",
+                Password = "robin",
+                Role = "employee"
+            });
+
+            var usuario = users.Where(x => x.UserName.ToLower() == username.ToLower() && x.Password.ToLower() == password.ToLower()).FirstOrDefault();
+
+            return usuario;
+        }
+    }
+}
